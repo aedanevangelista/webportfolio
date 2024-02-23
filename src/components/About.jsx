f8eb5ca6bf9d35f144a8650074abbd150eb6afdc
Aedan Evangelista <103922745+aedanevangelista@users.noreply.github.com>
2024-02-23 10:38:33 +0800

    new up

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,11 +8,14 @@ const About = () => {
     let timelineIcon = 'absolute  w-8 h-8 items-center flex justify-center rounded-full bg-sky-600 transition-all duration-300  text-white';
 
     let timelineContainer = "col-span-4 w-full h-full";
-    let timelineCardContainer = "w-full h-full hover:bg-sky-600/80 bg-sky-600 transition-all duration-300 rounded-sm p-2 md:pl-4";
+    let timelineCardContainer = "w-full h-full hover:bg-sky-600/95 bg-sky-500 transition-all duration-300 rounded-sm p-2 md:pl-4";
 
     let timelineH1 = "text-white text-2xl font-black mt-2";
     let timelineH2 = 'font-bold text-sky-300 text-lg';
     let timelineP = "text-gray-100 py-2 sm:text-sm text-xs font-semibold";
+    
+    let timelineCardDate = "sm:hidden block font-bold text-sm text-sky-200 w-44 text-center bg-slate-900/30 shadow-md shadow-black/50 my-2";
+
     
   return (
     <div name='about' className='w-full h-full py-20 bg-slate-900/[99%] text-gray-400'>
@@ -24,7 +27,7 @@ const About = () => {
 
             <div className='max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4'>
 
-                <div className='sm:text-right text-3xl font-semibold items-center'>
+                <div className='sm:text-right text-2xl font-semibold items-center'>
                   <p>Hi. I'm Aedan, nice to meet you! Please take a look around!</p>
                 </div>
 
@@ -44,7 +47,7 @@ const About = () => {
                 
                 {/* 1ST COL */}
 
-                <div className='hidden md:block col-span-4 w-full h-full'>
+                <div className='hidden md:block col-span-4 w-full h-full hover:scale-105 transition-all duration-500'>
                     <div className={timelineCardContainer}>
                         <h1 className={timelineH1}>GAME DEVELOPER & DESIGNER</h1>
                         <h2 className={timelineH2}>Skydev Solutions Inc.</h2>
@@ -63,6 +66,7 @@ const About = () => {
                         <h1 className={timelineH1}>GAME DEVELOPER & DESIGNER</h1>
                         <h2 className={timelineH2}>Skydev Solutions Inc.</h2>
                         <p className={timelineP}>My first job as a developer creating games with the Roblox Platform that is fun for all ages.</p>
+                        <p className={timelineCardDate}>Nov 2020 - Oct 2022</p>
                     </div>
                 </div>
 
@@ -76,11 +80,12 @@ const About = () => {
                     <div className={timelineIcon}><FaLaptop /></div>
                 </div>
 
-                <div className="col-span-8 sm:col-span-4 w-full h-full py-4">
+                <div className="col-span-8 sm:col-span-4 w-full h-full py-4 sm:hover:scale-105 transition-all duration-500">
                     <div class={timelineCardContainer}>
                         <h1 class={timelineH1}>FREELANCE DEVELOPER</h1>
                         <h2 className={timelineH2}>Ottodot Inc.</h2>
                         <p class={timelineP}>A part-time gig that helped me pay my tuition fee for college. Ensuring that I finish all the client's task at the given deadline.</p>
+                        <p className={timelineCardDate}>April 2021 - Oct 2021</p>
                     </div>
                 </div>
 
@@ -88,7 +93,7 @@ const About = () => {
                 {/* 3RD COL */}
 
 
-                <div className="hidden sm:block col-span-4 w-full h-full">
+                <div className="hidden sm:block col-span-4 w-full h-full hover:scale-105 transition-all duration-500">
                     <div className={timelineCardContainer}>
                         <h1 className={timelineH1}>TECHNICAL SUPPORT</h1>
                         <h2 className={timelineH2}>VXI Global Solutions, LLC</h2>
@@ -107,6 +112,7 @@ const About = () => {
                         <h1 className={timelineH1}>TECHNICAL SUPPORT</h1>
                         <h2 className={timelineH2}>VXI Global Solutions, LLC</h2>
                         <p className={timelineP}>Life went downwards as I fell into depression. Managed to pick myself up again and tried to become a technical support for the meantime. Got to improve my english communication skills that I still use up to this date as a developer.</p>
+                        <p className={timelineCardDate}>Oct 2022 - June 2023</p>
                     </div>
                 </div>
 
@@ -118,11 +124,12 @@ const About = () => {
                     <div className="h-full w-0.5 bg-sky-500"></div>
                     <div className={timelineIcon}><FaClipboardCheck /></div>
                 </div>
-                <div className="sm:col-span-4 col-span-8 w-full h-full py-4">
+                <div className="sm:col-span-4 col-span-8 w-full h-full py-4 sm:hover:scale-105 transition-all duration-500">
                     <div className={timelineCardContainer}>
                         <h1 className={timelineH1}>FREELANCE QA ENGINEER</h1>
                         <h2 className={timelineH2}>Aloautta Inc.</h2>
                         <p className={timelineP}>Came back to work after a break, feeling refreshed from focusing on my studies. Now, I'm diving into the world of Web Development with the support of awesome mentors at Alouatta.</p>
+                        <p className={timelineCardDate}>Aug 2023 - Oct 2023</p>
                     </div>
                 </div>
                 
@@ -130,7 +137,7 @@ const About = () => {
                 {/* 5TH COL */}
 
 
-                <div className="hidden sm:block col-span-4 w-full h-full ">
+                <div className="hidden sm:block col-span-4 w-full h-full hover:scale-105 transition-all duration-500">
                     <div className={timelineCardContainer}>
                     <h1 className={timelineH1}>QA ENGINEER</h1>
                         <h2 className={timelineH2}>Skydev Solutions Inc.</h2>
@@ -148,6 +155,7 @@ const About = () => {
                     <h1 className={timelineH1}>QA ENGINEER</h1>
                         <h2 className={timelineH2}>Skydev Solutions Inc.</h2>
                         <p className={timelineP}>Got back right on the track as a developer at my first company! But this time, I want to focus more on web development rather than game development!</p>
+                        <p className={timelineCardDate}>Oct 2023 - Present</p>
                     </div>
                 </div>
                 
